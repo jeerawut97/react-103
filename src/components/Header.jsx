3204692cf8536583d001b856d3f0a20f9b0fe5e8
JEE <jeerawut.jd40@gmail.com>
2023-12-09 11:44:28 +0700

    Setup Tailwind CSS & Intro

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -44,9 +44,12 @@ const StyledHeader = styled.header`
 
 export default function Header() {
   return (
-    <StyledHeader>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-    </StyledHeader>
+    // <StyledHeader>
+    <header className='flex flex-col items-center mt-8 mb-16'>
+      <img src={logo} alt="A canvas" className='object-contain mb-8 w-44 h-44' />
+      <h1 className='text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title'>ReactArt</h1>
+      <p>A community of artists and art-lovers.</p>
+    </header>
+    // </StyledHeader>
   );
 }
